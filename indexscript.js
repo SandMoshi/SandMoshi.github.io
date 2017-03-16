911f47112eed036df61e6af351d66b7ac4c3d113
Sand Moshi <sandmoshi@gmail.com>
2017-03-15 23:10:33 -0600

    Added banner image. Added Audio.

--- indexscript.js
+++ indexscript.js
@@ -1,2 +1,30 @@
 //Don't allow deactived links to work
 document.querySelector('a.deactive').style.pointerEvents="none";
+
+//Audio Player
+function toggleAudio(e){
+		const play = document.querySelector('.play');
+		const speaker = document.querySelector('.speaker');
+//		const pause = document.querySelector('.pause');
+	
+		const audio = document.querySelector('audio');
+		if (!audio.paused){
+			audio.pause();
+			play.src = "img/play-24.png";
+			speaker.src="img/mute-2-24 (1).png";
+		}
+		else{
+			// audio.currentTime = 0; //rewind
+			play.src = "img/media-pause-24.png";
+			speaker.src = "img/volume-up-4-24.png";
+			audio.play();
+		}
+}
+
+		const audio = document.querySelector('audio').addEventListener("ended",function(){
+			 	const play = document.querySelector('.play');
+				const speaker = document.querySelector('.speaker');
+				play.src = "img/play-24.png";
+				speaker.src="img/mute-2-24 (1).png";
+		});
+		
