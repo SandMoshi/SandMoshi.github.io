3278ddaff0756889732f73cf56eedc0e767407f1
Sand Moshi <sandmoshi@gmail.com>
2017-04-24 23:27:06 -0600

    Added responsiveness, doesn't repeat if possible, added foods

--- FoodPicker/script.js
+++ FoodPicker/script.js
@@ -7,39 +7,50 @@ else{
 }
 var button = document.querySelector('button.go');
 	console.log(button);
+var seen = []; //empty array to keep track of seen foods
 
+//----------
+function clearSeen(){
+	//empty the seen array
+	seen = [];
+	console.log("seen array cleared");
+}
+//---------
+function hideText(placeholder){
+	if(placeholder.classList.contains("visible")){
+		placeholder.classList.remove("visible");
+	}			 
+}
+function showText(placeholder,item){
+		placeholder.innerHTML = item;
+		placeholder.classList.add("visible");
+}
 //----------
 function updateApp(item){
 	var placeholder = document.querySelector("h2.food");
-	if(placeholder.classList.contains("visible")){
-		placeholder.classList.remove("visible");
-	}
-	
-	
+	hideText(placeholder);
 	setTimeout(function(){
-		placeholder.innerHTML = item;
-		placeholder.classList.add("visible")},300
-	);
+		showText(placeholder,item);
+	},300);
 }
-
 
 //----------
 function getFood(){
 	
 	//find out what the person selected
 	var fresh = document.querySelector('input.fresh').checked;
-		console.log("fresh:" + fresh);
+		//console.log("fresh:" + fresh);
 	var temp = document.querySelector('input.temp').checked;
 		if(temp === true){
 			temp = "hot";
 		}
 		else { temp = "cold";}
-		console.log("temp:" + temp);
+		//console.log("temp:" + temp);
 	
 	
 	//filter your foods list based on their selections
 	var filtered = foods.filter(function(food){
-		console.log(fresh);
+		//		console.log(fresh);
 		if(food.isfresh === fresh && food.temp === temp){
 			return true;
 		}
@@ -47,14 +58,43 @@ function getFood(){
 	console.log(filtered);
 	
 	var chosen = filtered[Math.floor(Math.random()*filtered.length)];
-	console.log("A food was chosen...");
 	console.log(chosen);
+	//make sure that the food hasn't been shown
+	if (!doubleCheck(chosen.food,filtered.length)){
+			console.log("repicking..")
+			getFood();
+			return;
+		}
+	console.log("A food was chosen...");
 	
 	updateApp(chosen.food); // this function will update DOM
-	
-}; //end of function
+} //end of function
 
+//----------
+function doubleCheck(item,total){
+	console.log(seen);
+	if (seen.indexOf(item) === -1){
+		seen.push(item); //new item so okay to show it
+		return true;
+	}
+	else if (seen.length === total){
+		var lastitem = seen[seen.length - 1];
+		console.log(lastitem);
+		clearSeen(); //empty the seen array and start over
+		console.log("all the foods have been exhausted");
+		if(lastitem === item){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	else{ 
+		return false; //this item has been shown, pick again
+	}
+};
 
 button.onclick = function(){
 	getFood();
 };
+
