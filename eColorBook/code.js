a01e53e6a6af9e18086c8f0afa5d9d2394eae001
Sand Moshi <sandmoshi@gmail.com>
2017-05-15 19:29:54 -0600

    Added touch support, resizing support

--- eColorBook/code.js
+++ eColorBook/code.js
@@ -1,11 +1,12 @@
 const canvas = document.querySelector("canvas#draw");
-
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
 const ctx = canvas.getContext('2d');
 
 //canvas.width = window.innerWidth;
 //canvas.height = window.innerHeight;
 
-ctx.strokeStyle = "black";
+ctx.strokeStyle = "grey";
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 12;
@@ -14,22 +15,52 @@ let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 
+// Prevent scrolling when touching the canvas
+document.body.addEventListener("touchstart", function (e) {
+	console.log(e.target);
+  if (e.target === canvas) {
+		document.body.classList.add("locked"); //this prevents scrolling
+		//		console.log("prevented scrolling");
+  }
+}, false);
+document.body.addEventListener("touchend", function (e) {
+  if (e.target === canvas) {
+		document.body.classList.remove("locked"); //this allows scrolling
+		//		console.log("prevented scrolling");
+  }
+}, false);
+
 function drawline(e){
 //	console.log(isDrawing);
 	if(!isDrawing){
 		return; //do nothing if not drawing
 	}
-	ctx.beginPath();
-	ctx.moveTo(lastX,lastY);
-	ctx.lineTo(e.offsetX, e.offsetY);
-	ctx.stroke();
-	lastX = e.offsetX;
-	lastY = e.offsetY;
+	if(e.offsetX === undefined){
+		var rect = e.target.getBoundingClientRect();
+		//		console.log(e.targetTouches[0].pageX);
+		ctx.beginPath();
+		ctx.moveTo(lastX,lastY);
+		lastX = e.targetTouches[0].pageX - rect.left;
+		lastY = e.targetTouches[0].pageY - rect.top;
+		ctx.lineTo(lastX,lastY);
+		ctx.stroke();
+		lastX = e.targetTouches[0].pageX - rect.left;
+		lastY = e.targetTouches[0].pageY - rect.top;
+	}
+	else{
+//		console.log(e);
+		ctx.beginPath();
+		ctx.moveTo(lastX,lastY);
+		ctx.lineTo(e.offsetX, e.offsetY);
+		ctx.stroke();
+		lastX = e.offsetX;
+		lastY = e.offsetY;
+	}
 }
 //Listen for mouse actions over canvas
 canvas.addEventListener('mousemove', drawline);
 canvas.addEventListener('mousedown', function(e){
-	//	console.log(e);
+//		console.log(e);
 	isDrawing = true
 	lastX = e.offsetX;
 	lastY = e.offsetY;
@@ -40,6 +71,26 @@ canvas.addEventListener('mouseup', function(){
 	load_image();
 });
 canvas.addEventListener('mouseout', function(){isDrawing = false});
+//Add the same event listeners but for touch devices now
+canvas.addEventListener('touchmove',function(e){
+	isDrawing = true
+	var rect = e.target.getBoundingClientRect();
+	lastX = e.targetTouches[0].pageX - rect.left;
+	lastY = e.targetTouches[0].pageY - rect.top;
+	drawline(e);
+	});
+canvas.addEventListener('touchstart', function(e){
+	//	console.log(e);
+	isDrawing = true
+	var rect = e.target.getBoundingClientRect();
+	lastX = e.targetTouches[0].pageX - rect.left;
+	lastY = e.targetTouches[0].pageY - rect.top;
+	drawline(e);
+});
+canvas.addEventListener('touchend', function(){
+	isDrawing = false;
+	load_image();
+});
 
 //Listen for color change
 let colors = document.querySelectorAll("div.pallete div.color a");
@@ -108,6 +159,6 @@ function load_image(){
 	let base = new Image();
 	base.src = 'pages/' + filename;
 	base.onload = function(){
-		ctx.drawImage(base, 0 , 0);
+		ctx.drawImage(base, 0 , 0, canvas.width, canvas.height);
 	}
 }
