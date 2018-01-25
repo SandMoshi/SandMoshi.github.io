e0a0b25269b7c897a3d9c1b9b2e8d6d0cb0d6f21
Sand Moshi <sandmoshi@gmail.com>
2018-01-24 21:25:20 -0700

    Changed audio player. Added CTA. Removed some cards

--- indexscript.js
+++ indexscript.js
@@ -1,5 +1,9 @@
 //Don't allow deactived links to work
 document.querySelector('a.deactive').style.pointerEvents="none";
+const audio = document.querySelector('audio');
+const dot = document.getElementsByClassName("dot")[0];
+const bar = document.getElementsByClassName("bar")[0];
+
 
 //Audio Player
 function toggleAudio(e){
@@ -7,7 +11,7 @@ function toggleAudio(e){
 		const speaker = document.querySelector('.speaker');
 //		const pause = document.querySelector('.pause');
 	
-		const audio = document.querySelector('audio');
+		// const audio = document.querySelector('audio');
 		if (!audio.paused){
 			audio.pause();
 			play.src = "img/play-24.png";
@@ -21,10 +25,18 @@ function toggleAudio(e){
 		}
 }
 
-		const audio = document.querySelector('audio').addEventListener("ended",function(){
-			 	const play = document.querySelector('.play');
+		audio.addEventListener("ended",function(){
+					const play = document.querySelector('.play');
 				const speaker = document.querySelector('.speaker');
 				play.src = "img/play-24.png";
 				speaker.src="img/mute-2-24 (1).png";
 		});
-		
+
+audio.addEventListener("timeupdate", function(){
+	var currentTime = audio.currentTime;
+	const duration = audio.duration;
+	var percentPlayed = currentTime/duration;
+	var barWidth = bar.offsetWidth;
+	var px = percentPlayed*(barWidth - 20);
+	dot.style.left = px + "px";
+});
