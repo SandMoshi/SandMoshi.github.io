bc469f1ee506a9a5a01ec4acc570a266d91cd92c
Sand Moshi <sandmoshi@gmail.com>
2019-11-17 20:17:39 -0500

    Added Escape Card. Removed transform scale. Changed deactivated button colour.

---
+++ Escape/precache-manifest.c4ea35bfac090e5706f2e076e1dc3671.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf84009de0ec5cebce9f3ce8b64410e",
+    "url": "/Escape/index.html"
+  },
+  {
+    "revision": "6b59722bed181cad81c8",
+    "url": "/Escape/static/css/main.ebf5ea82.chunk.css"
+  },
+  {
+    "revision": "a65b3d18e09d3357dc60",
+    "url": "/Escape/static/js/2.1efc1111.chunk.js"
+  },
+  {
+    "revision": "6b59722bed181cad81c8",
+    "url": "/Escape/static/js/main.e1c16461.chunk.js"
+  },
+  {
+    "revision": "be0a3741fad25f623c93",
+    "url": "/Escape/static/js/runtime-main.1fc15ef6.js"
+  },
+  {
+    "revision": "dd347b8b0fbc59b152334e5cfdbd9282",
+    "url": "/Escape/static/media/3270SemiNarrow.dd347b8b.ttf"
+  },
+  {
+    "revision": "8283cf1c750e8935789da1b0e42d91c4",
+    "url": "/Escape/static/media/Angel_of_Mercy.8283cf1c.mp3"
+  },
+  {
+    "revision": "724337e41fac1a0721a75d64ed08c632",
+    "url": "/Escape/static/media/Arcade.724337e4.mp3"
+  },
+  {
+    "revision": "174fe7ce9e8c5a63488071b4a8d4186a",
+    "url": "/Escape/static/media/Background_1.174fe7ce.png"
+  },
+  {
+    "revision": "750f41919f107f891c2379cf8a2c3f88",
+    "url": "/Escape/static/media/Choose_Your_Path.750f4191.mp3"
+  },
+  {
+    "revision": "4cb3072b7124bb9499a6115a88ad1722",
+    "url": "/Escape/static/media/Dark_Times.4cb3072b.mp3"
+  },
+  {
+    "revision": "e643d7857ed3a0feda16c68b711591fb",
+    "url": "/Escape/static/media/Echoes_of_Time.e643d785.mp3"
+  },
+  {
+    "revision": "ebecfef7d1dca6f18a696a9c1d469864",
+    "url": "/Escape/static/media/Echoes_of_Time_v2.ebecfef7.mp3"
+  },
+  {
+    "revision": "a3ded39de8e0f0ac811fcb8395504497",
+    "url": "/Escape/static/media/Faceoff.a3ded39d.mp3"
+  },
+  {
+    "revision": "93f3577aa8407f09361af6d624dd3221",
+    "url": "/Escape/static/media/Greenscr.93f3577a.ttf"
+  },
+  {
+    "revision": "e45452757b27635ce127374951631357",
+    "url": "/Escape/static/media/JailCellClose_combined.e4545275.mp3"
+  },
+  {
+    "revision": "3d917467a6b73e3636d9534a60f99794",
+    "url": "/Escape/static/media/Plato_s_Cave.3d917467.mp3"
+  },
+  {
+    "revision": "c7ebee722db9047aa0aa6e199a47bb9e",
+    "url": "/Escape/static/media/Scraping_The_Sewer.c7ebee72.mp3"
+  },
+  {
+    "revision": "a916e93a3dcb7af96701d2aaab937bf7",
+    "url": "/Escape/static/media/The_Deal_is_Going_Down.a916e93a.mp3"
+  },
+  {
+    "revision": "9fc2afef810e06af1c9f01cb0c6c680e",
+    "url": "/Escape/static/media/daytripper-sos-mayday.9fc2afef.mp3"
+  },
+  {
+    "revision": "9c87a43bfe045c52c25776d6f74afafe",
+    "url": "/Escape/static/media/kwahmah-stations-broadcasting_noise-reduced.9c87a43b.mp3"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Escape/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "816b7a835676baa1e32b5286bb965993",
+    "url": "/Escape/static/media/morse-30wps.816b7a83.mp3"
+  },
+  {
+    "revision": "5f313c9f01477e1d53b8d8f2d429321c",
+    "url": "/Escape/static/media/phone-position-rotation-from-horizontal-to-vertical.5f313c9f.svg"
+  },
+  {
+    "revision": "269793fa46b0b727803d46c2d7dd21a4",
+    "url": "/Escape/static/media/timbre_telecom_heavily_edited.269793fa.mp3"
+  },
+  {
+    "revision": "e81d045802a782210354f96fa8ad0604",
+    "url": "/Escape/static/media/trebblofang__creepy_background_transmission.e81d0458.mp3"
+  },
+  {
+    "revision": "49842426e31f781017f07528f463a625",
+    "url": "/Escape/static/media/trebblofang_pi_short.49842426.mp3"
+  }
+]);
